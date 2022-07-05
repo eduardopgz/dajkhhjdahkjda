58f370c70b0bca19192453a5a6ebd6e51b991789
Eduardo Paiva <eduardopgz@yahoo.com>
2022-07-05 10:44:12 -0300

    fazendo a atividae

---
+++ atividae.js
@@ -0,0 +1,16 @@
+
+let peso = 50;
+let listaQtdPecas = 9;
+let nomePeca = "pneu";
+
+if(peso >= 100){
+    console.log("Pode cadastrar.");
+}
+
+if(listaQtdPecas >=10){
+    console.log("Lista cheia.")    
+}
+
+if(nomePeca.length < 3){
+    console.log("Nome inválido!");
+}
